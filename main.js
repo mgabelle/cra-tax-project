451dd3fbbe7de8ef571670851894dc2f565b563a
Mathieu Gabelle <mathieu.gabelle@adevinta.com>
2023-05-09 09:50:00 -0400

    Implement website for tax project
    Add the tax fetcher in a html file
    
    Add notification when time is below 30 minutes

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+const TIME = "time";
+
 async function fetchTime() { return fetch("https://www.canada.ca/api/assets/cra-arc/content-fragments/wait-times.json");}
 
 async function getTime() {
-  return fetchTime().then(res => res.json()).then(res => 		         res.properties.elements.individual_en.value);
+  return fetchTime().then(res => res.json()).then(res => res.properties.elements.individual_en.value);
 }
 
 function timeToMinute(time) {
@@ -29,12 +31,59 @@ function timeToMinute(time) {
   return hourInt * 60 + minInt;
 }
 
-async function logTime() {
-  var time = await getTime();
-  var date = new Date();
-   console.log(date.toLocaleTimeString());
-   console.log("Wait time: " + time + " (min:" + timeToMinute(time) +")");
-   console.log("\n");
+function notifyMe(time) {
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  } else if (Notification.permission === "granted") {
+    createNewNotification(time);
+  } else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        createNewNotification(time);
+      }
+    });
+  }
+}
+
+function createNewNotification(time) {
+  new Notification(`CRA wait time is ${time}`);
+}
+
+
+async function getTimes() {
+  const time = await getTime()
+  return {
+    time: time,
+    date: new Date().toLocaleTimeString()
+  }
  }
 
-setInterval(logTime, 300000);
+ async function logTime() {
+  const timeObject = await getTimes();
+  const {time, date} = timeObject;
+
+  localStorage.setItem(TIME, time)
+
+  const timeTable = document.getElementById('timeTable');
+
+  const row = timeTable.insertRow(0);;
+  const timeTd = document.createElement('td');
+  const dateTd = document.createElement('td');
+
+  timeTd.textContent = time;
+  dateTd.textContent = date;
+
+  row.appendChild(timeTd);
+  row.appendChild(dateTd);
+ }
+
+logTime();
+
+setInterval(logTime, 60000);
+
+setInterval(() => {
+  const time = localStorage.getItem(TIME);
+  if (timeToMinute(time) < 30) {
+    notifyMe(time);
+  }
+}, 60000 * 10)
